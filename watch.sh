0ac07d6c299a45d93fa2759a38cdde62f2589580
mujtaba <mujtaba1747@gmail.com>
2020-09-12 02:02:37 +0530

    "Updated watch.sh"

--- watch.sh
+++ watch.sh
@@ -2,7 +2,7 @@
 path="/home/mujtaba/Desktop/GitBois/MyDrive" # Replace this with the path to your git repository. To get path, run pwd in the directory or right click and select properties of the folder  
 while true 
 do
-	inotifywait --recursive -qq -e attrib,create,delete,modify,delete_self,move,move_self,close_write $path > /dev/null &> /dev/null
+	inotifywait --recursive -qq -e attrib,create,delete,modify,delete_self,move,move_self,close_write $path #> /dev/null &> /dev/null
 	cd $path > /dev/null &> /dev/null
 	git add --all > /dev/null &> /dev/null
 	git commit -m "Auto-Commit at : " &> /dev/null
