515c4cf58af56abe5759e002e6940438ae687dd0
mujtaba <mujtaba1747@gmail.com>
2020-09-12 01:02:04 +0530

    Auto-Commit at :

--- watch.sh
+++ watch.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-path="/home/username/Desktop/MyDrive" # Replace this with the path to your git repository. To get path, run pwd in the directory or right click and select properties of the folder  
+path="/home/username/Desktop/GitBois/MyDrive" # Replace this with the path to your git repository. To get path, run pwd in the directory or right click and select properties of the folder  
 while true 
 do
-	inotifywait --recursive -qq -e attrib,create,delete,modify,delete_self,move,move_self,close_write $path
+	inotifywait --recursive -qq -e attrib,create,delete,modify,delete_self,move,move_self,close_write $path > /dev/null &> /dev/null
 	cd $path > /dev/null &> /dev/null
 	git add --all > /dev/null &> /dev/null
 	git commit -m "Auto-Commit at : " &> /dev/null
 	git pull > /dev/null &> /dev/null
-	git push -u origin master &> /dev/null
+	git push -u origin master > /dev/null &> /dev/null
 done
