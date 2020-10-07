dae9a6036d09b72818eea48e8d1fad3eb3a1448d
Pratik Daigavane <39241098+pratikdaigavane@users.noreply.github.com>
2020-10-07 08:53:49 +0530

    Update watch.sh
    changing the order of git commands

--- watch.sh
+++ watch.sh
@@ -4,9 +4,9 @@ while true
 do
 	inotifywait --recursive -qq -e attrib,create,delete,modify,delete_self,move,move_self,close_write $path
 	cd $path                              > /dev/null &> /dev/null
+	git pull                              > /dev/null &> /dev/null
 	git add --all                         > /dev/null &> /dev/null
 	now=$(date)                           > /dev/null &> /dev/null
-	git commit -m "Auto-Commit at : $now" > /dev/null &> /dev/null
-	git pull                              > /dev/null &> /dev/null
+	git commit -m "Auto-Commit at : $now" > /dev/null &> /dev/null	
 	git push -u origin master             > /dev/null &> /dev/null
 done
