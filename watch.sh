751fdca39736f480b93df5ffadf7a8f4da97e365
mujtaba <mujtaba@pop-os.localdomain>
2021-04-14 03:53:15 +0530

    Changed script to push changes to main instead of master

--- watch.sh
+++ watch.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-path="/home/mujtaba/Desktop/Repo-Name" # Replace this with the path to your git repository. To get path, run pwd in the directory or right click and select properties of the folder  
-while true 
+path="/home/mujtaba/Desktop/Repo-Name" # Replace this with the path to your git repository. To get path, run pwd in the directory or right click and select properties of the folder
+while true
 do
 	inotifywait --recursive -qq -e attrib,create,delete,modify,delete_self,move,move_self,close_write $path
-	cd $path                              > /dev/null &> /dev/null
-	git pull                              > /dev/null &> /dev/null
-	git add --all                         > /dev/null &> /dev/null
-	now=$(date)                           > /dev/null &> /dev/null
-	git commit -m "Auto-Commit at : $now" > /dev/null &> /dev/null	
-	git push -u origin master             > /dev/null &> /dev/null
+	cd $path                              &> /dev/null
+	git pull                              &> /dev/null
+	git add --all                         &> /dev/null
+	now=$(date)                           &> /dev/null
+	git commit -m "Auto-Commit at : $now" &> /dev/null	
+	git push -u origin main               &> /dev/null
 done
